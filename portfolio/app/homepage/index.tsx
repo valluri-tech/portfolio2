2dd44d437f8b0a27a0f14050c96eb10192a0b267
vallurisatyaASA <Veeravenkata.Valluri@AirservicesAustralia.com>
2024-04-08 18:51:51 +1000

    Profile picture positioning updated for Mobile

--- portfolio/app/homepage/index.tsx
+++ portfolio/app/homepage/index.tsx
@@ -1,13 +1,32 @@
 "use client";
 import Image from "next/image";
 import { Box } from "@mui/material";
-
-const imageStyle = {
-  borderRadius: "50%",
-  border: "1px solid #fff",
-};
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function HomePage() {
+  const mobile = useMediaQuery("(max-width:480px)");
+
+  const imageStyle = {
+    borderRadius: "50%",
+    my: "8rem",
+    position: mobile ? "absolute" : "static",
+    top: mobile ? "10px" : "",
+    right: mobile ? "10px" : "",
+  };
+
+  if (mobile) {
+    return (
+      <Image
+        src={"/profile.jpg"}
+        width={100}
+        height={100}
+        alt="Picture of the Prakash"
+        style={imageStyle}
+        priority={true}
+      />
+    );
+  }
+
   return (
     <Box sx={{ my: "8rem" }}>
       <Image
